50bc83d5066da3f1c0b965f576271d789b049c68
Paul Hammant <paul@hammant.org>
2020-04-06 12:03:06 +0100

    class centric

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-https://github.com/servirtium/README/blob/master/starting-a-new-implementation.md - currently at step 1
+# Servirtium demo for Javascript
+
+This repo is following the guide at https://github.com/servirtium/README/blob/master/starting-a-new-implementation.md 
+
+- currently, step 1 is complete and step 2 is next
+
+## Yarn installs
+
+```
+yarn add --dev jest
+yarn add --dev jquery
+```
+
+## Run tests
+
+```
+yarn test
+```
--- climateAPI.test.js
+++ climateAPI.test.js
@@ -1,21 +1,23 @@
-const getAveAnnualRainfall = require('./climateAPI');
+ClimateAPI = require('./climateAPI');
+
+const climateAPI = new ClimateAPI();
 
 test('average Rainfall For Great Britain From 1980 to 1999 Exists', () => {
-  expect(getAveAnnualRainfall(1980, 1999, "gbr")).toBeCloseTo(988.8454972331015);
+  expect(climateAPI.getAveAnnualRainfall(1980, 1999, "gbr")).toBeCloseTo(988.8454972331015);
 });
 
 test('average Rainfall For France From 1980 to 1999 Exists', () => {
-  expect(getAveAnnualRainfall(1980, 1999, "fra")).toBeCloseTo(913.7986955122727);
+  expect(climateAPI.getAveAnnualRainfall(1980, 1999, "fra")).toBeCloseTo(913.7986955122727);
 });
 
 test('average Rainfall For Egypt From 1980 to 1999 Exists', () => {
-  expect(getAveAnnualRainfall(1980, 1999, "egy")).toBeCloseTo(54.58587712129825);
+  expect(climateAPI.getAveAnnualRainfall(1980, 1999, "egy")).toBeCloseTo(54.58587712129825);
 });
 
 test('average Rainfall For Great Britain From 1985 to 1995 Does Not Exists', () => {
-  expect(getAveAnnualRainfall(1985, 1995, "gbr")).toBe("no data for date range");
+  expect(climateAPI.getAveAnnualRainfall(1985, 1995, "gbr")).toBe("no data for date range");
 });
 
 test('average Rainfall For Middle Earth From 1980 to 1999 Exists', () => {
-  expect(getAveAnnualRainfall(1980, 1999, "mde")).toBe("Invalid country code. Three letters are required");
+  expect(climateAPI.getAveAnnualRainfall(1980, 1999, "mde")).toBe("Invalid country code. Three letters are required");
 });
